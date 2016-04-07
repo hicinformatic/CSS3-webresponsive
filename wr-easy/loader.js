521a5e63dc7d8b2282a36707ac8b7d3953c86689
Charles Delencre <charles.delencre.ext@hubone.fr>
2016-04-07 18:50:08 +0200

    sizing global ok

---
+++ wr-easy/loader.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+//begin
+
+// var rollandunroll = function() {
+//     var attrDisplay = this.nextElementSibling.style.display;
+//     this.nextElementSibling.style.display = attrDisplay == "block" ? "none" : "block";
+
+// };
+
+// var classname = document.getElementsByClassName("wr-button-roll");
+// for (var i = 0; i < classname.length; i++) {
+//     classname[i].addEventListener('click', rollandunroll, false);
+// }
+
+//end
+});
